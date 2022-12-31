eb42b030afba640ef97719f8d06777917273f21e
davidponc <buap.david@gmail.com>
2022-12-31 13:36:23 -0600

    chore: disable eslint rule

--- src/hoc/withTheme.js
+++ src/hoc/withTheme.js
@@ -1,5 +1,6 @@
 import { ThemeContextProvider } from 'context/ThemeContext';
 
+// eslint-disable-next-line react/display-name
 export const withTheme = Page => props => {
 	return (
 		<ThemeContextProvider>
